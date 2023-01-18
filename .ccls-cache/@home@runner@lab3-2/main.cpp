0d2c554f518bfef421d0b3590f0694c11b271cbc
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-18 02:18:24 +0000

    add input statement for user add celsius conversion display temp in fahrenheit and celsius

--- .ccls-cache/@home@runner@lab3-2/main.cpp
+++ .ccls-cache/@home@runner@lab3-2/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main() 
+{
+  double fahr_temp, cels_temp;
+  const double CELSIUS_CONVERSION = (5/9);
+  
+  //input fahrenheit temp
+  cout << "Enter temperature in fahrenheit" << endl;
+  cin >> fahr_temp;
+  
+  //convert from fahrenheit to Celsius
+  cels_temp = (CELSIUS_CONVERSION) * (fahr_temp - 32);
+  
+  //output both the fahrenheit and Celsius temperatures neatly formatted
+  cout << "Fahrenheit = " << fahr_temp << "\nCelsius = " << cels_temp << endl;
+  
+  return 0;
 }
