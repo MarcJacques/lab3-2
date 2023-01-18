648f075fb69e932eecb98ce50baa4442edc1a44a
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-18 02:27:08 +0000

    add decimal numbers to arithmetic operations

--- .ccls-cache/@home@runner@lab3-2/main.cpp
+++ .ccls-cache/@home@runner@lab3-2/main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main() 
 {
   double fahr_temp, cels_temp;
-  const double CELSIUS_CONVERSION = (5/9);
-  
+  const double CELSIUS_CONVERSION = 5.0/9.0;
+  // cout << CELSIUS_CONVERSION;
   //input fahrenheit temp
   cout << "Enter temperature in fahrenheit" << endl;
   cin >> fahr_temp;
   
   //convert from fahrenheit to Celsius
-  cels_temp = (CELSIUS_CONVERSION) * (fahr_temp - 32);
+  cels_temp = (CELSIUS_CONVERSION) * (fahr_temp - 32.0);
   
   //output both the fahrenheit and Celsius temperatures neatly formatted
   cout << "Fahrenheit = " << fahr_temp << "\nCelsius = " << cels_temp << endl;
